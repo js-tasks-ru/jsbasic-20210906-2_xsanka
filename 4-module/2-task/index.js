1f3391aa0ef9038448e32376b1ca5064fd2a9a79
“kdemeneva” <k.demeneva@tradedealer.ru>
2021-09-24 20:21:07 +0500

    4 module

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
-function makeDiagonalRed(table) {
-  // ваш код...
+function makeDiagonalRed(table) {      
+
+    let trs = table.querySelectorAll('tr');
+    trs.forEach(function(item, index, array) {
+        let tds = item.querySelectorAll('td');
+        tds.forEach(function(td, i) {
+            if (i == index) {
+                td.style.backgroundColor = "red";
+            }
+        });
+    });      
+
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,61 @@
 function highlight(table) {
-  // ваш код...
-}
+        let thead = table.querySelector('thead');
+        let tbody = table.querySelector('tbody');
+        let tbodyTrs = tbody.querySelectorAll('tr');
+        let theadTrs = thead.querySelectorAll('tr');
+
+        
+        tbodyTrs.forEach(function(item) {
+            let tds = item.querySelectorAll('td');
+            let noDataset = true;
+            
+            tds.forEach(function(td) {
+                if (td.dataset.available === 'true') {
+                    td.parentNode.classList.add('available');
+                    noDataset = false;
+                } else if (td.dataset.available === 'false') {
+                    td.parentNode.classList.add('unavailable');
+                    noDataset = false;
+                }
+            });
+            
+            if (noDataset) {
+                item.setAttribute('hidden', '');
+            }  
+        });
+
+        
+        let genderTd;
+        let ageTd;
+        theadTrs.forEach(function(item) {
+            let tds = item.querySelectorAll('td');
+            tds.forEach(function(td,i) {                
+                if (td.innerHTML.includes('Gender')) {
+                    genderTd = i;
+                }
+                if (td.innerHTML.includes('Age')) {
+                    ageTd = i;
+                }                
+            });
+        });
+        
+        
+        tbodyTrs.forEach(function(item) {
+            let tds = item.querySelectorAll('td');
+            
+            
+            tds.forEach(function(td,i) {
+                if (tds[genderTd].innerHTML.includes('m')) {
+                    td.parentNode.classList.add('male');
+                } else if (tds[genderTd].innerHTML.includes('f')) {
+                    td.parentNode.classList.add('female');
+                }
+
+                if (Number(tds[ageTd].innerHTML) < 18) {
+                    td.parentNode.style.textDecoration = 'line-through';
+                }
+                
+            });            
+            
+        });
+    }
